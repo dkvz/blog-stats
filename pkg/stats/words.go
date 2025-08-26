28e43f17e84f61b78b730718018d72f26279a9c6
dkvz <github@dkvz.eu>
2025-08-26 15:26:46 +0200

    word counting alg almost ready

--- pkg/stats/words.go
+++ pkg/stats/words.go
@@ -9,7 +9,10 @@ import (
 var paReg = regexp.MustCompile(`</?(p|h\d|a|i|b|small|strike|sub|sup|abbr|blockquote).*?>`)
 
 // To use at the end to clean up the remaining tags
-var tagsReg = regexp.MustCompile(`<.+?>.+?</.+?>`)
+// var tagsReg = regexp.MustCompile(`<.+?>.+?</.+?>`)
+var tagsReg = regexp.MustCompile(`<[^>]*>[^>]*<[^>]*>`)
+
+var consSpaceReg = regexp.MustCompile(`\s{2,}`)
 
 func WordCount(content string) int {
 	// Remove all <p> and </p>
@@ -17,15 +20,16 @@ func WordCount(content string) int {
 	// Remove img and svg, some tags may not have closing element
 	// Remove most tags and their inner content
 	// We can keep blockquote contents
-	res := strings.TrimSpace(paReg.ReplaceAllString(content, ""))
+	res := paReg.ReplaceAllString(content, "")
 	res = tagsReg.ReplaceAllString(res, "")
 	res = strings.ReplaceAll(res, "\n", " ")
 	res = strings.ReplaceAll(res, "&nbsp;", " ")
 
+	// Remove consecutive spaces
+	res = strings.TrimSpace(consSpaceReg.ReplaceAllString(res, " "))
+
 	fmt.Println(res)
 
-	// Remove consecutive spaces
-
-	// Count spaces and add 1
+	// Count spaces
 	return strings.Count(res, " ") + 1
 }
