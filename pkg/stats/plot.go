48263e450070960be45a6a9af8d5b118d0462da3
dkvz <github@dkvz.eu>
2025-08-31 20:13:59 +0200

    prepare file for plot function

---
+++ pkg/stats/plot.go
@@ -0,0 +1,8 @@
+package stats
+
+// Functions here are more generic versions
+// of what runtime will be using.
+
+// We need to be able to generate scatter plots
+// And the runtime decides whether to generate
+// images or run a local HTTP server.
